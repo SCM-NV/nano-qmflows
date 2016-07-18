b174abf6f3c0af9fff7c83ab2f6c03081187d29a
Roandinho <roanvl@int2.cartesius.surfsara.nl>
2016-07-18 15:54:36 +0200

    some debugging after file was rewritten

--- scripts/spectralAnalysis.py
+++ scripts/spectralAnalysis.py
@@ -10,9 +10,9 @@ import os
 import sys
 
 
-msg = """This is a program that plots the spectral density for a certain
-pair of states.Usage: Make sure that you are in the out folder containing the
-icond-files and fill in the prompted questions."""
+msg = ('This is a program that plots the spectral density for a certain'
+'pair of states. Usage: Make sure that you are in the out folder containing the'
+'icond-files and fill in the prompted questions.')
 
 
 def ask_question(q_str, special=None, default=None):
@@ -67,9 +67,9 @@ def ask_question(q_str, special=None, default=None):
 def ask_for_states():
     print("""The states are labeled as in PYXAID; the first state being 0.
     You can look them up in the output.""")
-    i1 = ask_question("What is the integer representing the first state (int)?",
+    i1 = ask_question("What is the integer representing the first state (int)? ",
                       special='int')
-    i2 = ask_question("What is the integer representing the second state (int)?",
+    i2 = ask_question("What is the integer representing the second state (int)? ",
                       special='int')
     return i1, i2
 
@@ -97,45 +97,60 @@ def read_files(i1, i2):
             j.append(arr[1])
         return w, j
     else:
-        msg = """File not found.\nAre you in the out folder? And are \
-        you sure the ints are correct?\n.\
-        The program was looking for a file named: {} in your \
-        current directory.""".format(filename)
+        name2 = name[0:4]+'0'+name[6:]
+        msg = ('File not found.\nAre you in the out folder? And are '
+        'you sure the ints are correct?\n'
+        'The program was looking for a file named: \'{}\' in your '
+        'current directory.'.format(name2))
         raise FileNotFoundError(msg)
 
 
-def plot_stuff(w, J, m1, m2, xl='w [cm^-1]', yl='J', save_plot=False):
+def plot_stuff(w, J, m1, m2, xl='w [cm^-1]', yl='J [arb. units]', save_plot=False):
     """
-    Document me...
+    function to plot
+    
+    takes:
+    w - list of np-vectors containing x-values (energy, in cm-1) for each initial condition
+    J - list of np-vectors containing y-values (J) for each initial condition
+    xl - string containing the label on the x-axis
+    yl - string containing the label on the y-axis
+    save_plot - bool describing if plot should be saved as .pdf or not
+
+    does:
+    1) ask user for a maximum for the x-axis
+    2) prepares the graph for plotting, including labels etc
+    3) if wanted, saves plot
+    4) shows the plot
     """
+
     question = "What is the maximal value of w (in cm^-1) that you want \
     to plot (float/int)? [Default: highest value] "
     
-    maxw = ask_question(question, special='float', default=np.max(J[0]))
-    filename = "SpecDens_{:d}_{:d}.pdf".forma(m1, m2)
+    maxw = ask_question(question, special='float', default=w[-1])
+    filename = "SpecDens_{:d}_{:d}.pdf".format(m1, m2)
     with PdfPages(filename) as pdf:
         for i in range(len(J)):
-            plt.plot(w, J[i], label='icond{:d}'.format(i))
+            plt.plot(w[i], J[i], label='icond{:d}'.format(i))
             plt.xlabel(xl)
             plt.ylabel(yl)
-            plt.xlim(w[0], maxw)
+            plt.xlim(w[0][0], maxw)
             plt.legend()
-            if save_plot:
-                pdf.savefig()
-                d = pdf.infodict()
-                d['Title'] = 'Spectral Density Figure '
-                d['Author'] = 'Autmotically created'
-                d['Subject'] = 'SpecDens PDF'
-                d['Keywords'] = 'spectral density'
-                d['CreationDate'] = datetime.datetime.today()
-                d['ModDate'] = datetime.datetime.today()
-            plt.show()
+        if save_plot:
+            pdf.savefig()
+            d = pdf.infodict()
+            d['Title'] = 'Spectral Density Figure '
+            d['Author'] = 'Autmotically created'
+            d['Subject'] = 'SpecDens PDF'
+            d['Keywords'] = 'spectral density'
+            d['CreationDate'] = datetime.datetime.today()
+            d['ModDate'] = datetime.datetime.today()
+        plt.show()
 
 
 def main():
     i1, i2 = ask_for_states()
     question = "Do you want to save the plot as SpecDens_{}_{}.pdf (y/n)? \
-    [Default: n] ".forma(i1, i2)
+    [Default: n] ".format(i1, i2)
     save_fig = ask_question(question, special='bool', default=False)
     w, J = read_files(i1, i2)
     plot_stuff(w, J, i1, i2, save_plot=save_fig)
