b6c9ec4cc916e8901b1b19ab573aa398497c09ea
Roandinho <roanvanleeuwen@gmail.com>
2016-07-20 17:33:23 +0200

    interact with user via questions, now in separate file

---
+++ scripts/Analysis/interactive.py
@@ -0,0 +1,24 @@
+def ask_question(q_str, special='None', default=None):
+    """
+    function that ask the question, and parses the answer to prefered format
+    q_str = string containing the question to be asked
+    returns string, bool (spec='bool'), int (spec='int') or float (spec='float')
+    """
+
+    import sys
+    
+    while True:
+        question = str(input(q_str)) if sys.version_info[0] == 3 else str(raw_input(q_str))
+        funcs = {'None': [str, {}], 'bool': [bool, {'y': True, 'yes': True, 'n': False, 'no': False}], 'int': [int, {}], 'float': [float, {}] }
+        
+        if not question and default:
+            question = str(default)
+        if question in funcs[special][1]:
+            return funcs[special][1][question]
+        elif special is not 'bool':
+            try:
+                return funcs[special][0](question)
+            except ValueError:
+                pass
+        else:
+            print("Input not recognised. Please try again.")
