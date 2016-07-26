a8f991eeb2e5a17fd73308e9cebf030a6d4a8db2
felipe zapata <tifonzafel@gmail.com>
2016-07-26 16:56:01 +0200

    fixed on oscillator strength calculator

--- nac/integrals/absorptionSpectrum.py
+++ nac/integrals/absorptionSpectrum.py
@@ -95,11 +95,15 @@ def  oscillator_strength(atoms, cgfsN, css_i, css_j, energy, trans_mtx):
     rc = calculateDipoleCenter(atoms, cgfsN, css_i, overlap_sum, trans_mtx)
 
     print("Dipole center is: ", rc)
-    mtx_integrals = [calcMtxMultipoleP(atoms, cgfsN, rc, **kw)
-                     for kw in exponents]
-
+    mtx_integrals_triang = [calcMtxMultipoleP(atoms, cgfsN, rc, **kw)
+                            for kw in exponents]
+    mtx_integrals_cart = [triang2mtx(xs, dimCart)
+                          for xs in mtx_triang_cart]
+    mtx_integrals_spher = [transform2Spherical(x, trans_mtx) for x
+                           in mtx_integrals_cart]
     sum_integrals = sum(lambda x: x ** 2,
                         map(partial(computeIntegralSum, css_i_T, css_j),
-                            mtx_integrals))
+                            mtx_integrals_spher))
 
     return (2 / 3) * energy * sum_integrals
+
