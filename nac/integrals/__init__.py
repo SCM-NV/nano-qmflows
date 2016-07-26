43ec3502b3a69d7c50ebaadf64221165492d91de
felipe zapata <tifonzafel@gmail.com>
2016-07-26 12:25:36 +0200

    fixed bug in import statement

--- nac/integrals/__init__.py
+++ nac/integrals/__init__.py
@@ -1,5 +1,5 @@
-from .absorptionSpectrum.py import (oscillator_strength, calculateDipoleCenter)
-from .electronTransfer.py import photoExcitationRate
+from .absorptionSpectrum import (oscillator_strength, calculateDipoleCenter)
+from .electronTransfer import photoExcitationRate
 from .nonAdiabaticCoupling import calculateCoupling3Points
 from .overlapIntegral import *
 from .spherical_Cartesian_cgf import calc_transf_matrix
