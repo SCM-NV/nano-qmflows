ca68945439f4dd92c89df7ef6dd99d08901b99bd
felipe zapata <tifonzafel@gmail.com>
2016-08-09 15:24:33 +0200

    fixed bug importing multipoleIntegrals

--- nac/integrals/__init__.py
+++ nac/integrals/__init__.py
@@ -1,5 +1,5 @@
 from .electronTransfer import photoExcitationRate
-from .multipoleObaraSaika import (general_multipole_matrix, calcMtxMultipoleP)
+from .multipoleIntegrals import (calcMtxMultipoleP, general_multipole_matrix)
 from .nonAdiabaticCoupling import calculateCoupling3Points
 from .overlapIntegral import calcMtxOverlapP
 from .spherical_Cartesian_cgf import calc_transf_matrix
