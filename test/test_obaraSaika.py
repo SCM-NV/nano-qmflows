bfdee89d004c75e38418aef9d9c664cb5c995762
felipe zapata <tifonzafel@gmail.com>
2016-09-21 14:12:26 +0200

    added test for obaraSaika integration and the derivative couplings that depend on them

---
+++ test/test_obaraSaika.py
@@ -0,0 +1,60 @@
+
+from nac import (calculateCoupling3Points, initialize)
+from nac.common import (femtosec2au, retrieve_hdf5_data)
+from os.path import join
+from qmworks.parsers import parse_string_xyz
+
+import numpy as np
+import os
+
+path_hdf5 = 'test/test_files/ethylene.hdf5'
+path_hdf5_test = 'test/test_files/test.hdf5'
+path_traj_xyz = 'test/test_files/threePoints.xyz'
+
+
+def create_paths_mos(project_name, i):
+    """
+    create the path to the nodes containing the MOs inside the HDF5.
+    """
+    root = join(project_name, 'point_{}'.format(i), 'cp2k', 'mo')
+
+    return [join(root, 'eigenvalues'), join(root, 'coefficients')]
+
+
+def test_obaraSaika():
+    project_name = 'ethylene'
+    home = os.path.expanduser('~')
+    basisname = "DZVP-MOLOPT-SR-GTH"
+    basiscp2k = join(home, "Cp2k/cp2k_basis/BASIS_MOLOPT")
+    potcp2k = join(home, "Cp2k/cp2k_basis/GTH_POTENTIALS")
+
+    initial_config = initialize(project_name, path_traj_xyz,
+                                basisname=basisname, path_basis=basiscp2k,
+                                path_potential=potcp2k,
+                                enumerate_from=0,
+                                scratch='/tmp',
+                                path_hdf5=path_hdf5_test,
+                                calculate_guesses='first')
+
+    string_geoms = initial_config['geometries']
+    geometries = tuple(map(parse_string_xyz, string_geoms))
+
+    dictCGFs = initial_config['dictCGFs']
+    hdf5_trans_mtx = initial_config['hdf5_trans_mtx']
+    trans_mtx = retrieve_hdf5_data(path_hdf5_test, hdf5_trans_mtx)
+    trans_mtx = trans_mtx[3:43, :]
+    dt_au = femtosec2au
+
+    mo_paths = [create_paths_mos(project_name, i) for i in range(3)]
+    mos = tuple(map(lambda j:
+                    retrieve_hdf5_data(path_hdf5,
+                                       mo_paths[j][1]), range(3)))
+
+    mos = [xs[3:43, 3:43] for xs in mos]
+    print(mos[0].shape, trans_mtx.shape)
+    rs = calculateCoupling3Points(geometries, mos, dictCGFs, dt_au,
+                                  trans_mtx)
+
+    expected = retrieve_hdf5_data(path_hdf5, join(project_name, 'coupling_0'))
+
+    assert np.sum(rs - expected) < 1e-7
