aceb989e6a632ec7a6971d3a0cd4b795ac6aee5e
felipe <tifonzafel@gmail.com>
2016-11-13 17:27:36 +0100

    removed fun_density_real from init

--- nac/integrals/__init__.py
+++ nac/integrals/__init__.py
@@ -1,6 +1,6 @@
 from .electronTransfer import photoExcitationRate
 from .fourierTransform import (calculate_fourier_trasform_cartesian,
-                               fun_density_real, real_to_reciprocal_space,
+                               real_to_reciprocal_space,
                                transform_to_spherical)
 from .multipoleIntegrals import (calcMtxMultipoleP, general_multipole_matrix)
 from .nonAdiabaticCoupling import calculateCoupling3Points
@@ -10,6 +10,6 @@ from .spherical_Cartesian_cgf import calc_transf_matrix
 
 __all__ = ['calc_transf_matrix', 'calcMtxMultipoleP', 'calcMtxOverlapP',
            'calculateCoupling3Points', 'calculate_fourier_trasform_cartesian',
-           'fun_density_real', 'general_multipole_matrix',
+           'general_multipole_matrix',
            'photoExcitationRate', 'real_to_reciprocal_space',
            'transform_to_spherical']
