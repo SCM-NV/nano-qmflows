760d67eeb5b688035020388da97ff6bcd7986a92
felipe zapata <tifonzafel@gmail.com>
2016-11-16 15:27:09 +0100

    removed spherical transformation from the Fourier transform (#14)

--- nac/integrals/__init__.py
+++ nac/integrals/__init__.py
@@ -1,7 +1,6 @@
 from .electronTransfer import photoExcitationRate
 from .fourierTransform import (calculate_fourier_trasform_cartesian,
-                               real_to_reciprocal_space,
-                               transform_to_spherical)
+                               real_to_reciprocal_space)
 from .multipoleIntegrals import (calcMtxMultipoleP, general_multipole_matrix)
 from .nonAdiabaticCoupling import calculateCoupling3Points
 from .overlapIntegral import calcMtxOverlapP
@@ -11,5 +10,4 @@ from .spherical_Cartesian_cgf import calc_transf_matrix
 __all__ = ['calc_transf_matrix', 'calcMtxMultipoleP', 'calcMtxOverlapP',
            'calculateCoupling3Points', 'calculate_fourier_trasform_cartesian',
            'general_multipole_matrix',
-           'photoExcitationRate', 'real_to_reciprocal_space',
-           'transform_to_spherical']
+           'photoExcitationRate', 'real_to_reciprocal_space']
