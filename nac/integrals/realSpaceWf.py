baf75637ba791faac65cbc958f7616e66b08ca57
ProkopHapala <ProkopHapala@gmail.com>
2016-12-02 13:00:46 +0100

    comments to realSpaceWf.py

--- nac/integrals/realSpaceWf.py
+++ nac/integrals/realSpaceWf.py
@@ -17,6 +17,7 @@ angular_momenta_dict = {
 
 def chiRealSpace(Xs, Ys, Zs, cgfs, coefs):
     """
+    evaluate contribution from basis functions of particular atom in points (Xs,Ys,Zs) centred arround that atom
     """
     R2 = Xs ** 2 + Ys ** 2 + Zs ** 2
     wf = np.zeros(Xs.shape)
@@ -25,7 +26,7 @@ def chiRealSpace(Xs, Ys, Zs, cgfs, coefs):
         label  = cgf.orbType
         chi = np.zeros(Xs.shape)
         for c, eta in zip(cs, es):
-            chi += np.exp(-eta * R2)   # this is the slowest part
+            chi += np.exp(-eta * R2)   # this is the slowest part - can be probably optimized by precomputing chi(r) and using scipy.interp1d()  
         L = angular_momenta_dict[label]
         for j in range(L[0]):
             chi *= Xs
@@ -34,7 +35,6 @@ def chiRealSpace(Xs, Ys, Zs, cgfs, coefs):
         for j in range(L[2]):
             chi *= Zs
         wf  += chi * coefs[i]
-
     return wf
 
 
@@ -58,6 +58,11 @@ def writeArr2D(f, arr):
 
 
 def atoms_to_file(f, lvec, symbols, coords):
+    '''
+    write atomic position in XCrysDen format; see: http://www.xcrysden.org/doc/XSF.html
+     "lvec" is 4x3 matrix [origin,a,b,c] where origin is begining of grid in global cartesian space 
+        and (a,b,c) is matrix of lattice vectors which transforms from lattice to cartesian coordinates 
+    '''
     f.write("CRYSTAL\n")
     f.write("PRIMVEC\n")
     writeArr2D(f, lvec)
@@ -72,6 +77,9 @@ def atoms_to_file(f, lvec, symbols, coords):
 
 def saveXSF(fname, data, lvec, head=XSF_HEAD_DEFAULT, symbols=None, coords=None):
     """
+    save scalar 3D data grid "data" into XCrysDen format; see: http://www.xcrysden.org/doc/XSF.html
+     "lvec" is 4x3 matrix [origin,a,b,c] where origin is begining of grid in global cartesian space 
+            and (a,b,c) is matrix of lattice vectors which transforms from lattice to cartesian coordinates 
     """
     print("saving Xsf to ", fname)
     with open(fname, 'w') as fileout:
@@ -92,6 +100,13 @@ def saveXSF(fname, data, lvec, head=XSF_HEAD_DEFAULT, symbols=None, coords=None)
 
 def pre_wf_real(coords, Rcut=6.0, dstep=np.array((0.5, 0.5, 0.5))):
     """
+    prepare grid sampling "(Xs,Ys,Ys)" with sampling step "dstep" arround atoms in "coords"
+    the grid is sampled in box from "amin-Rcut" to "amax+Rcut" where amin,amax are corners of bounding box arround the atomic centers 
+    for each atom we save also index of corner-points of bounding box arround that atom with size "Rcut*2" to "indBounds" 
+        the boxes should contain all non-negligible contribution of basis functions centered on that atom 
+        WARNING : choose Rcut carefully, it should correspond to distance where slowest-decaying basis function is non-negligible
+                  if Rcut is two small importaint part of basis function can be ommited
+                  on the other hand if Rcut is large evaluation is very slow 
     """
     dstep_inv = 1 / dstep
     Rmargin = np.ones(3) * Rcut * 1.1
@@ -117,6 +132,10 @@ def pre_wf_real(coords, Rcut=6.0, dstep=np.array((0.5, 0.5, 0.5))):
 
 def wf_real(orbital, symbols, coords, dictCGFs, mo_i, XYZs, indBounds):
     """
+    evaluate molecualr orbital on grid sampling given in "XYZs"
+    for each atom we select a sub-grid determined by atomic bounding box indexes in "indBounds"
+    the sub-grid position samples (Xc,Yc,Zc) are shifted so that the atoms is in the center 
+    and evaluate chiRealSpace() for this shifted subgrid
     """
     (Xs, Ys, Zs) = XYZs
     (minInds, maxInds) = indBounds
