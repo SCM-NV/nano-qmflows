7601458a755f0d72fa9d02ae0718da7cf99201c5
felipe zapata <tifonzafel@gmail.com>
2017-04-10 18:11:42 +0200

    fixed import issue import the function calculating the coupling

--- nac/integrals/__init__.py
+++ nac/integrals/__init__.py
@@ -1,7 +1,7 @@
 from .electronTransfer import photoExcitationRate
 from .fourierTransform import (calculate_fourier_trasform_cartesian)
 from .multipoleIntegrals import (calcMtxMultipoleP, general_multipole_matrix)
-from .nonAdiabaticCoupling import (calculateCoupling3Points,
+from .nonAdiabaticCoupling import (calculate_couplings_levine,
                                    compute_overlaps_for_coupling,
                                    correct_phases)
 from .overlapIntegral import calcMtxOverlapP
@@ -9,6 +9,6 @@ from .spherical_Cartesian_cgf import calc_transf_matrix
 
 
 __all__ = ['calc_transf_matrix', 'calcMtxMultipoleP', 'calcMtxOverlapP',
-           'calculateCoupling3Points', 'calculate_fourier_trasform_cartesian',
+           'calculate_couplings_levine', 'calculate_fourier_trasform_cartesian',
            'compute_overlaps_for_coupling', 'correct_phases',
            'general_multipole_matrix', 'photoExcitationRate']
