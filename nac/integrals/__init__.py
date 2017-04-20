9bab50ecc5f05a0c23ee8010f3488c52a62b7033
felipe zapata <tifonzafel@gmail.com>
2017-04-20 11:00:01 +0200

    Fixed __init__ module for the integrals

--- nac/integrals/__init__.py
+++ nac/integrals/__init__.py
@@ -2,13 +2,15 @@ from .electronTransfer import photoExcitationRate
 from .fourierTransform import (calculate_fourier_trasform_cartesian)
 from .multipoleIntegrals import (calcMtxMultipoleP, general_multipole_matrix)
 from .nonAdiabaticCoupling import (calculate_couplings_levine,
+                                   calculate_couplings_3points,
                                    compute_overlaps_for_coupling,
                                    correct_phases)
 from .overlapIntegral import calcMtxOverlapP
 from .spherical_Cartesian_cgf import calc_transf_matrix
 
 
-__all__ = ['calc_transf_matrix', 'calcMtxMultipoleP', 'calcMtxOverlapP',
+__all__ = ['calculate_couplings_3points', 'calculate_couplings_levine',
+           'calc_transf_matrix', 'calcMtxMultipoleP', 'calcMtxOverlapP',
            'calculate_couplings_levine', 'calculate_fourier_trasform_cartesian',
            'compute_overlaps_for_coupling', 'correct_phases',
            'general_multipole_matrix', 'photoExcitationRate']
