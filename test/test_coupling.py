2ba0d8fdfcab935c9aa881c80cda7e10cf6df359
felipe zapata <tifonzafel@gmail.com>
2017-06-02 16:51:40 +0200

    modified test of the oscillator calculation

--- test/test_coupling.py
+++ test/test_coupling.py
@@ -92,6 +92,7 @@ def calculate_couplings_and_oscillators():
     data = workflow_oscillator_strength(
         'cp2k', project_name, cp2k_main, guess_args=cp2k_guess,
         nHOMO=50, couplings_range=(50, 30), initial_states=[49],
+        energy_range=(0, 5),  # eV
         final_states=[[51]], **initial_config)
 
     return data
