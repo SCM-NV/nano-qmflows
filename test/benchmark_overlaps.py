6d4f3cfa284c6684a507dc57857711a4aa3929d7
felipe zapata <tifonzafel@gmail.com>
2017-06-09 15:32:49 +0200

    fixed the benchmark for the overlaps

--- test/benchmark_overlaps.py
+++ test/benchmark_overlaps.py
@@ -1,4 +1,4 @@
-
+from functools import partial
 from nac.common import (
     Matrix, change_mol_units, getmass, retrieve_hdf5_data, triang2mtx)
 from nac.integrals.multipoleIntegrals import calcMtxMultipoleP
@@ -6,79 +6,78 @@ from nac.workflows.initialization import initialize
 from os.path import join
 from qmworks.parsers import parse_string_xyz
 from scipy import sparse
+from typing import (List, Tuple)
 
+import h5py
 import numpy as np
 import os
 import shutil
 
 
-from typing import (List, Tuple)
-
 basisname = 'DZVP-MOLOPT-SR-GTH'
-path_traj_xyz = 'Cd33Se33_fivePoints.xyz'
+path_traj_xyz = 'test/test_files/Cd33Se33_fivePoints.xyz'
 scratch_path = 'scratch'
-path_original_hdf5 = 'Cd33Se33.hdf5'
+path_original_hdf5 = 'test/test_files/Cd33Se33.hdf5'
 path_test_hdf5 = join(scratch_path, 'test.hdf5')
 project_name = 'Cd33Se33'
 
 
 def main():
-
-    copy_files()
-    config = initialize(
-        project_name, path_traj_xyz,
-        basisname=basisname, path_basis=None,
-        path_potential=None, enumerate_from=0,
-        calculate_guesses='first', path_hdf5=path_test_hdf5,
-        scratch_path=scratch_path)
-
-    # If the MO orbitals are given in Spherical Coordinates transform then to
-    # Cartesian Coordinates.
-    trans_mtx = retrieve_hdf5_data(path_test_hdf5, config['hdf5_trans_mtx'])
-    dictCGFs = config['dictCGFs']
-
-    # Molecular geometries
-    geometries = config['geometries']
-    molecule_at_t0 = change_mol_units(parse_string_xyz(geometries[0]))
-
-    # Contracted Gaussian functions normalized
-    cgfsN = [dictCGFs[x.symbol] for x in molecule_at_t0]
-
-    # Origin of the dipole
-    rc = compute_center_of_mass(molecule_at_t0)
-    mtx_integrals_spher = calcDipoleCGFS(molecule_at_t0, cgfsN, rc, trans_mtx)
-
-    print(tuple(map(lambda mtx: mtx.shape, mtx_integrals_spher)))
-
-
-def copy_files():
     if not os.path.exists(scratch_path):
         os.makedirs(scratch_path)
-        shutil.copy(path_original_hdf5, path_test_hdf5)
+    try:
+        shutil.copy('test/test_files/BASIS_MOLOPT', scratch_path)
+        shutil.copy('test/test_files/GTH_POTENTIALS', scratch_path)
+
+        # Run the actual test
+        copy_basis_and_orbitals(path_original_hdf5, path_test_hdf5,
+                                project_name)
+
+        config = initialize(
+            project_name, path_traj_xyz,
+            basisname=basisname, path_basis=None,
+            path_potential=None, enumerate_from=0,
+            calculate_guesses='first', path_hdf5=path_test_hdf5,
+            scratch_path=scratch_path)
+
+        # If the MO orbitals are given in Spherical Coordinates transform then to
+        # Cartesian Coordinates.
+        trans_mtx = retrieve_hdf5_data(path_test_hdf5, config['hdf5_trans_mtx'])
+        dictCGFs = config['dictCGFs']
+
+        # Molecular geometries
+        geometries = config['geometries']
+        molecule_at_t0 = change_mol_units(parse_string_xyz(geometries[0]))
+
+        # Contracted Gaussian functions normalized
+        cgfsN = [dictCGFs[x.symbol] for x in molecule_at_t0]
+
+        # Origin of the dipole
+        rc = compute_center_of_mass(molecule_at_t0)
+        mtx_integrals_spher = calcDipoleCGFS(molecule_at_t0, cgfsN, rc, trans_mtx)
+
+        print(tuple(map(lambda mtx: mtx.shape, mtx_integrals_spher)))
+    finally:
+        # remove tmp data and clean global config
+        shutil.rmtree(scratch_path)
 
 
+# @profile
 def calcDipoleCGFS(
         atoms: List, cgfsN: List, rc: Tuple, trans_mtx: Matrix) -> Matrix:
     """
-    Compute the Multipole matrix in cartesian coordinates and
-    expand it to a matrix and finally convert it to spherical coordinates.
-
-    :param atoms: Atomic label and cartesian coordinates in au.
-    type atoms: List of namedTuples
-    :param cgfsN: Contracted gauss functions normalized, represented as
-    a list of tuples of coefficients and Exponents.
-    type cgfsN: [(Coeff, Expo)]
-    :param trans_mtx: Transformation matrix to translate from Cartesian
-    to Sphericals.
-    :type trans_mtx: Numpy Matrix
-    :returns: tuple(<ψi | x | ψj>, <ψi | y | ψj>, <ψi | z | ψj> )
     """
     # x,y,z exponents value for the dipole
     exponents = [{'e': 1, 'f': 0, 'g': 0}, {'e': 0, 'f': 1, 'g': 0},
                  {'e': 0, 'f': 0, 'g': 1}]
 
     dimCart = trans_mtx.shape[1]
-    mtx_integrals_triang = tuple(calcMtxMultipoleP(atoms, cgfsN, rc, **kw)
+
+    # Partial application
+    partial_multipole = partial(calcMtxMultipoleP, atoms, cgfsN, rc)
+
+    # mtx_integrals_triang = tuple(calcMtxMultipoleP(atoms, cgfsN, rc, **kw)
+    mtx_integrals_triang = tuple(partial_multipole(**kw)
                                  for kw in exponents)
     mtx_integrals_cart = tuple(triang2mtx(xs, dimCart)
                                for xs in mtx_integrals_triang)
@@ -113,6 +112,23 @@ def compute_center_of_mass(atoms: List) -> Tuple:
     cm = xs / total_mass
 
     return tuple(cm)
+
+
+def copy_basis_and_orbitals(source, dest, project_name):
+    """
+    Copy the Orbitals and the basis set from one the HDF5 to another
+    """
+    keys = [project_name, 'cp2k']
+    excluded = ['coupling', 'overlaps', 'swaps']
+    with h5py.File(source, 'r') as f5, h5py.File(dest, 'w') as g5:
+        for k in keys:
+            if k not in g5:
+                g5.create_group(k)
+            for l in f5[k].keys():
+                if not any(x in l for x in excluded):
+                    path = join(k, l)
+                    f5.copy(path, g5[k])
+
 
 if __name__ == "__main__":
     main()
