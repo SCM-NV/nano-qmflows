782af8f5152f2d1c379b55e15d323491feb1f597
Ivan Infante <iinfant76@gmail.com>
2018-04-23 15:27:59 +0200

    Small bug fixed in __init__.py

--- nac/analysis/__init__.py
+++ nac/analysis/__init__.py
@@ -13,4 +13,4 @@ from .tools import (autocorrelate,
 __all__ = [
     'autocorrelate', 'dephasing', 'convolute', 'func_conv', 'gauss_function',
     'parse_list_of_lists', 'read_couplings', 'read_energies',
-    'read_energies_pyxaid, 'read_pops_pyxaid', 'spectral_density']
+    'read_energies_pyxaid', 'read_pops_pyxaid', 'spectral_density']
