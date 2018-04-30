24eff6017ca4084acb0da3ff8db0b2d71d247325
Ivan Infante <iinfant76@gmail.com>
2018-04-30 14:44:08 +0200

    Updated plot_cooling script

--- scripts/pyxaid/plot_cooling.py
+++ scripts/pyxaid/plot_cooling.py
@@ -22,41 +22,13 @@ from nac.analysis import (read_energies_pyxaid, read_pops_pyxaid, convolute, aut
 def func(x, a, b, c, d, e):
     return a * np.exp(- x ** 2 / b ** 2) + d * np.exp(- x / e) + c
 
-#def read_energies(path, fn, nstates, nconds):
-#    inpfile = os.path.join(path, fn)
-#    cols = tuple(range(5, nstates * 2 + 5, 2))
-#    xs = np.stack(np.loadtxt('{}{}'.format(inpfile, j), usecols=cols)
-#                  for j in range(nconds)).transpose()
-#    # Rows = timeframes ; Columns = states ; tensor = initial conditions
-#    xs = xs.swapaxes(0, 1)
-#    return xs
-
-
-#def read_pops(path, fn, nstates, nconds):
-#    inpfile = os.path.join(path, fn)
-#    cols = tuple(range(3, nstates * 2 + 3, 2))
-#    xs = np.stack(np.loadtxt('{}{}'.format(inpfile, j), usecols=cols)
-#                  for j in range(nconds)).transpose()
-#    # Rows = timeframes ; Columns = states ; tensor = initial conditions
-#    xs = xs.swapaxes(0, 1)
-#    return xs
-
-#def g(x_real, x_grid, delta):
-#    return np.exp(-2 * (x_grid - x_real) ** 2 / delta ** 2)
-
-#def convolute(x, y, x_points, sigma):
-#    # Compute gaussian prefactor
-#    prefactor = np.sqrt(2.0) / (sigma * np.sqrt(np.pi)) 
-#    # Convolute spectrum over grid 
-#    y_points = prefactor * np.stack(np.sum( y * g(x, x_point, sigma) ) for x_point in x_points)
-#    return y_points 
-
-def plot_stuff(ts, aven_conv, aven_scaled_conv, w_en, w_en_scaled, aven, minim, maxim, start, end, uacf, nacf, sd_int, sd_freq, dt):
+def plot_stuff(x_grid, y_grid, x_grid_scaled, y_grid_scaled, sd, w_en, w_en_scaled, nconds, outs, ts, dt):
 
     plt.figure(1)
     plt.xlabel('Time (fs)')
     plt.ylabel('Energy (eV)')
-    plt.imshow(aven_conv.T, aspect='auto', origin='lower', extent=(0, len(ts) * dt, minim, maxim), interpolation='bicubic', cmap='hot')
+    for iconds in range(nconds):
+        plt.imshow(y_grid[iconds, :, :].T, aspect='auto', extent=(0, len(ts)*dt, np.min(x_grid), np.max(x_grid)), origin='lower', interpolation='bicubic', cmap='hot')
     plt.plot(ts, w_en, 'w')
     interactive(True)
     plt.show()
@@ -64,113 +36,90 @@ def plot_stuff(ts, aven_conv, aven_scaled_conv, w_en, w_en_scaled, aven, minim, 
     plt.figure(2)
     plt.xlabel('Time (fs)')
     plt.ylabel('Scaled Energy (eV)')
-    plt.imshow(aven_scaled_conv.T, aspect='auto', origin='lower', extent=(0, len(ts) * dt, start, end), interpolation='bicubic', cmap='hot')
+    for iconds in range(nconds):
+        plt.imshow(y_grid_scaled[iconds, :, :].T, aspect='auto', extent=(0, len(ts)*dt, np.min(x_grid_scaled), np.max(x_grid_scaled)), origin='lower', interpolation='bicubic', cmap='hot')  
     plt.plot(ts, w_en_scaled, 'w')
     interactive(True)
     plt.show()
 
-    plt.figure(3)
+    plt.figure(3) 
+    plt.xlabel('Time (fs)')
+    plt.ylabel('State Number')
+    for iconds in range(nconds):
+        plt.imshow(outs[:, :, iconds].T, aspect='auto', origin='lower', extent = ( 0, len(ts) * dt, 0, outs.shape[1]), interpolation='bicubic', cmap='hot')    
+    interactive(True)
+    plt.show() 
+
+    plt.figure(4)
     plt.xlabel('Energy (eV)')
     plt.ylabel('Counts')
     plt.hist(w_en, bins='auto')
     interactive(True)
     plt.show()
 
-    plt.figure(4)
+    plt.figure(5)
     plt.xlabel('Energy (eV)')
     plt.ylabel('Counts')
     plt.hist(w_en_scaled, bins='auto')
     interactive(True)
     plt.show()
 
-    plt.figure(5)
-    plt.xlabel('Time')
-    plt.ylabel('Energy (eV)')
-    plt.plot(ts, aven)
-    interactive(True)
-    plt.show()
-
     plt.figure(6)
-    ts = np.arange(uacf.size) * dt
-    plt.xlabel('Un-normalized AUC')
-    plt.ylabel('Time')
-    plt.plot(ts, uacf)
-    interactive(True)
-    plt.show()
-
-    plt.figure(7)
-    plt.xlabel('Normalized AUC')
-    plt.ylabel('Time')
-    plt.plot(ts, nacf)
-    interactive(True)
-    plt.show()
-
-    plt.figure(8)
-    plt.xlabel('Spectral Density')
-    plt.ylabel('Freqencies cm-1')
-    plt.xlim(0, 300)
-    plt.ylim(0, np.max(sd_int))
-    plt.plot(sd_freq, sd_int)
+    plt.ylabel('State Number')
+    plt.xlabel('Freqencies cm-1')
+    sd_int = sd[:, 0, :int(sd.shape[2]/2)] 
+    sd_freq = sd[0, 1, :int(sd.shape[2]/2)]
+#    plt.ylim(0, np.max(sd_int))
+#    plt.xlim(0, 3000)
+    plt.imshow(sd_int, aspect='auto', origin='lower', extent = (np.min(sd_freq), np.max(sd_freq), 0, sd_int.shape[0] ), interpolation='bicubic', cmap='hot')    
     interactive(False)
     plt.show()
 
 
-def main(path_output, nstates, nconds, dt, start, end, sigma):
+def main(path_output, nstates, nconds, dt, sigma):
     outs = read_pops_pyxaid(path_output, 'out', nstates, nconds)
     energies = read_energies_pyxaid(path_output, 'me_energies', nstates, nconds)
     ##################################
-    # Averaged energies and populations over initial conditions
-    av_outs = np.average(outs, axis=2)
-    av_energies = np.average(energies, axis=2)
     # Remove the ground state and scale the energies to the lowest excitation energy
-    av_outs = av_outs[:, 1:]
-    av_energies = av_energies[:, 1:]
-    lowest_hl_gap = np.average(np.amin(energies[:, 1:, :], axis=1), axis=1)
-    highest_hl_gap = np.average(np.amax(energies[:, 1:, :], axis=1), axis=1)
-    av_energies_scaled = (av_energies.transpose() - lowest_hl_gap).transpose()
-    # Convolute a gaussian for each timestep
+    outs = outs[:, 1:, :]
+    energies = energies[:, 1:, :]
+    lowest_hl_gap = np.amin(energies, axis=1)
+    highest_hl_gap = np.amax(energies, axis=1)
+    # Convolute a gaussian for each timestep and for each initial condition
     x_grid = np.linspace(np.min(lowest_hl_gap), np.max(highest_hl_gap), 100)
     y_grid = np.stack(np.stack(
-        convolute(av_energies[time, :], av_outs[time, :], x_grid, sigma)
-        for time in range(av_energies.shape[0])))
-    x_grid_scaled = np.linspace(start, end, 100)
+        convolute(energies[time, :, iconds], outs[time, :, iconds], x_grid, sigma)
+        for time in range(energies.shape[0]) )
+        for iconds in range(nconds) )   
+    # Scale the energy for computing the excess energy plots
+    energies_scaled = np.stack(energies[:, istate, :] - lowest_hl_gap for istate in range(nstates-1)) 
+    energies_scaled = energies_scaled.swapaxes(0,1) # Just reshape to have the energies shape (time, nstates, nconds) 
+    x_grid_scaled = np.linspace(0, np.max(energies_scaled), 100)
     y_grid_scaled = np.stack(np.stack(
-        convolute(av_energies_scaled[time, :], av_outs[time, :], x_grid_scaled, sigma)
-        for time in range(av_energies_scaled.shape[0])))
+        convolute(energies_scaled[time, :, iconds], outs[time, :, iconds], x_grid_scaled, sigma)
+        for time in range(energies_scaled.shape[0]) )
+        for iconds in range(nconds) ) 
     # This part is done
     ##################################
     # Compute weighted energies vs population at a given time t
     eav_outs = energies * outs
-    # Ensamble average over initial conditions of the electronic energy
-    # as a function of time
-    el_ene_outs = np.average(np.sum(eav_outs, axis=1), axis=1)
+    el_ene_outs = np.sum(eav_outs, axis=1)
     # Scale them to the lowest excitation energy
     ene_outs_ref0 = el_ene_outs - lowest_hl_gap
-    # Compute autocorrelation function of the weighted energy
-    uacf, nacf = autocorrelate(ene_outs_ref0)
+    #Average over initial conditions 
+    ene_outs_ref0 = np.average(ene_outs_ref0, axis=1) 
+    el_ene_av = np.average(el_ene_outs, axis = 1) 
+    ##################################
+    # Compute autocorrelation function for consecutive pair of states 
+    d_en = np.stack(energies[:, istate, 0] - energies[:, istate-1, 0] for istate in range(1, nstates-1))
+    acf = np.stack(autocorrelate(d_en[istate, :]) for istate in range(d_en.shape[0]))
     # Compute spectral density
-    sd_int, sd_freq = spectral_density(nacf, dt)
+    nacf = acf[:, 1, :]
+    sd = np.stack(spectral_density(nacf[istate, :], dt) for istate in range(d_en.shape[0]))
     #################################
-    # Print the 3d-array on a file for plotting
-    xs = ""
-    for time in range(av_energies_scaled.shape[0]):
-        for x_point in range(x_grid.size):
-            xs += '{:f} {:f} {:f} \n'.format(time, x_grid[x_point], y_grid[time, x_point])
-    with open('cooling_plot_data.txt', 'w') as f:
-        f.write(xs)
-    # Plotting stuff
-    # Define size axes for all plot
-    ts = np.arange(av_energies.shape[0]) * dt
     # Call plotting function
-    plot_stuff(ts, y_grid, y_grid_scaled, el_ene_outs, ene_outs_ref0, av_energies, np.min(lowest_hl_gap), np.max(highest_hl_gap), start, end, uacf, nacf, sd_int, sd_freq, dt)
-
-#    plt.plot(ts, ene_outs_ref0, 'w')
-#    plt.show()
-#    plt.hist(el_ene_outs[1000:], bins='auto')
-#    plt.show()
-#    plt.imshow(av_outs.T, aspect='auto', origin='lower', extent=(0, av_energies.shape[0] * dt, 0, av_energies.shape[1] * dt), interpolation='bicubic', cmap='hot')
-#    plt.show() 
-
+    ts = np.arange(energies.shape[0])
+    plot_stuff(x_grid, y_grid, x_grid_scaled, y_grid_scaled, sd, el_ene_av, ene_outs_ref0, nconds, outs, ts, dt)
 
 def read_cmd_line(parser):
     """
@@ -178,7 +127,7 @@ def read_cmd_line(parser):
     """
     args = parser.parse_args()
 
-    attributes = ['p', 'nstates', 'nconds', 'dt', 'start', 'end', 'sigma']
+    attributes = ['p', 'nstates', 'nconds', 'dt', 'sigma']
 
     return [getattr(args, p) for p in attributes]
 
@@ -199,10 +148,6 @@ if __name__ == "__main__":
                         help='Number of initial conditions')
     parser.add_argument('-dt', type=float, required=False, default=1.0,
                         help='Nuclear Time Step')
-    parser.add_argument('-start', type=float, required=False, default=0.0,
-                        help='Start point for the convolution on the energy axis')
-    parser.add_argument('-end', type=float, required=False, default=3.0,
-                        help='End point for the convolution on the energy axis')
     parser.add_argument('-sigma', type=float, required=False, default=0.05,
                         help='Line broadening for the convolution with Gaussian functions')
     main(*read_cmd_line(parser))
