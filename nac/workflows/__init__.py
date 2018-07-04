53bc172729c81175f3ea3f4d9c19a803ee4307c3
Ivan Infante <iinfant76@gmail.com>
2018-07-04 16:42:05 +0200

    Added new workflow to compute overlap dephasing

--- nac/workflows/__init__.py
+++ nac/workflows/__init__.py
@@ -2,8 +2,9 @@ from .initialization import (
     create_map_index_pyxaid, initialize, store_transf_matrix)
 from .workflow_AbsortionSpectrum import workflow_oscillator_strength
 from .workflow_coupling import generate_pyxaid_hamiltonians
+from .workflow_overlap_dephasing import generate_overlap_dephasing 
 from .workflow_cube import workflow_compute_cubes
 
-__all__ = ['create_map_index_pyxaid', 'generate_pyxaid_hamiltonians',
+__all__ = ['create_map_index_pyxaid', 'generate_pyxaid_hamiltonians', 'generate_overlap_dephasing', 
            'initialize', 'store_transf_matrix', 'workflow_compute_cubes',
            'workflow_oscillator_strength']
