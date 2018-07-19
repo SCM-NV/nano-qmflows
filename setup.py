e83ea01e8ec574909624facb3c7062b44c5cb9e8
Felipe Zapata <tifonzafel@gmail.com>
2018-07-19 17:32:44 +0200

    added coverage to dependencies

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     ext_modules=[Extension(
         'multipoleObaraSaika', ['nac/integrals/multipoleObaraSaika.pyx'])],
     include_dirs=[np.get_include()],
-    extras_require={'test': ['pytest', 'pytest-cov']},
+    extras_require={'test': ['coverage', 'pytest', 'pytest-cov']},
     scripts=[
         'scripts/hamiltonians/plot_mos_energies.py',
         'scripts/hamiltonians/plot_spectra.py',
