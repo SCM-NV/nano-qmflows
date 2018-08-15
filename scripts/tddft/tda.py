177ceda4eec86301680ad04d2ca7148225923510
Ivan Infante <iinfant76@gmail.com>
2018-08-15 23:10:03 +0200

    Improved tda module with adding functions. Hardness dictionary moved to common.py

--- scripts/tddft/tda.py
+++ scripts/tddft/tda.py
@@ -1,42 +1,21 @@
 import h5py
-from nac.basisSet import create_dict_CGFs
 from qmflows.parsers.xyzParser import readXYZ
-from nac.common import change_mol_units
+from nac.basisSet import create_dict_CGFs
+from nac.integrals.spherical_Cartesian_cgf import (calc_orbital_Slabels, read_basis_format)  
+from nac.common import (change_mol_units, hardness, h2ev)
 
 import numpy as np 
 import subprocess 
 from scipy.linalg import sqrtm
 from scipy.spatial.distance import cdist
 
-
-def hardness(s: str):
-    d = {'h': 6.4299, 'he': 12.5449, 'li': 2.3746, 'be': 3.4968, 'b': 4.619, 'c': 5.7410, 'n': 6.6824, 'o': 7.9854,
-         'f': 9.1065, 'ne': 10.2303, 'na': 2.4441, 'mg': 3.0146, 'al': 3.5849, 'si': 4.1551, 'p': 4.7258,
-         's': 5.2960, 'cl': 5.8662, 'ar': 6.4366, 'k': 2.3273, 'ca': 2.7587, 'sc': 2.8582, 'ti': 2.9578,
-         'v': 3.0573, 'cr': 3.1567, 'mn': 3.2564, 'fe': 3.3559, 'co': 3.4556, 'ni': 3.555, 'cu': 3.6544,
-         'zn': 3.7542, 'ga': 4.1855, 'ge': 4.6166, 'as': 5.0662, 'se': 5.4795, 'br': 5.9111, 'kr': 6.3418,
-         'rb': 2.1204, 'sr': 2.5374, 'y': 2.6335, 'zr': 2.7297, 'nb': 2.8260, 'mo': 2.9221, 'tc': 3.0184,
-         'ru': 3.1146, 'rh': 3.2107, 'pd': 3.3069, 'ag': 3.4032, 'cd': 3.4994, 'in': 3.9164, 'sn': 4.3332,
-         'sb': 4.7501, 'te': 5.167, 'i': 5.5839, 'xe': 6.0009, 'cs': 0.6829, 'ba': 0.9201, 'la': 1.1571,
-         'ce': 1.3943, 'pr': 1.6315, 'nd': 1.8686, 'pm': 2.1056, 'sm': 2.3427, 'eu': 2.5798, 'gd': 2.8170,
-         'tb': 3.0540, 'dy': 3.2912, 'ho': 3.5283, 'er': 3.7655, 'tm': 4.0026, 'yb': 4.2395, 'lu': 4.4766,
-         'hf': 4.7065, 'ta': 4.9508, 'w': 5.1879, 're': 5.4256, 'os': 5.6619, 'ir': 5.900, 'pt': 6.1367,
-         'au': 6.3741, 'hg': 6.6103, 'tl': 1.7043, 'pb': 1.9435, 'bi': 2.1785, 'po': 2.4158, 'at': 2.6528,
-         'rn': 2.8899, 'fr': 0.9882, 'ra': 1.2819, 'ac': 1.3497, 'th': 1.4175, 'pa': 1.9368, 'u': 2.2305,
-         'np': 2.5241, 'pu': 3.0436, 'am': 3.4169, 'cm': 3.4050, 'bk': 3.9244, 'cf': 4.2181, 'es': 4.5116,
-         'fm': 4.8051, 'md': 5.0100, 'no': 5.3926, 'lr': 5.4607 }
-    return d[s]
-
-def n_sph(s: str):
-    d = {'cd': 25, 'se': 13}
-    return d[s] 
-
 # Some basic input variables
 project_name = 'Cd33Se33_QD'
+package_name = 'cp2k'
 path_hdf5 = 'Cd33Se33.hdf5' 
 basisname='DZVP-MOLOPT-SR-GTH'
-path_overlap = 'overlap_Cd33Se33_cp2k.npy'
-path_tdm = 'tdm_Cd33Se33_cp2k.npy'
+path_overlap = 'overlap_Cd33Se33_cp2k.npy' # This wont be needed. Now just for debugging 
+path_tdm = 'tdm_Cd33Se33_cp2k.npy' # This wont be needed. Now just for debugging. 
 mol_file = 'Cd33Se33.xyz'
 
 # Some basic input variable for the sTDA calculations
@@ -46,45 +25,66 @@ alpha2 = 0.48 # These values are fitted by Grimme (2013)
 beta1 = 0.2 # These values are fitted by Grimme (2013)
 beta2 = 1.83 # These values are fitted by Grimme (2013)
 
-
 # Loading main stuff
+#### Call the calculate MOs function 
 f5 = h5py.File(path_hdf5, 'r') # Open the hdf5 file with MOs and energy values 
 c_ao = f5['{}/point_0/cp2k/mo/coefficients'.format(project_name)].value # Read MOs coefficients in AO basis. Matrix size: NAO x NMO 
 e = f5['{}/point_0/cp2k/mo/eigenvalues'.format(project_name)].value
-s = np.load(path_overlap) # Load Overlap matrix in AO basis 
+
+### Call the function that computes overlaps  
+# Check if overlaps are stored
+# If not
+mol = change_mol_units(readXYZ(mol_file))
+dictCGFs = create_dict_CGFs(path_hdf5, basisname, mol) 
+n_cart_funcs = np.sum(np.stack(len(dictCGFs[atoms[i]]) for i in range(n_atoms)))
+rs = calcMtxOverlapP(mol, dictCGFs) 
+mtx_overlap = triang2mtx(rs, n_cart_funcs)  # there are 1452 Cartesian basis CGFs
+dict_global_norms = compute_normalization_sphericals(dictCGFs)
+
+with h5py.File(path_hdf5, 'r') as f5:
+    transf_mtx = calc_transf_matrix(
+        f5, mol, basisname, dict_global_norms, 'cp2k')
+
+transf_mtx = sparse.csr_matrix(transf_mtx)
+transpose = transf_mtx.transpose()
+
+s = transf_mtx.dot(sparse.csr_matrix.dot(mtx_overlap, transpose))
+# Write them in hdf5 
+
+#s = np.load(path_overlap) # Load Overlap matrix in AO basis 
+
+### Call the function that computes transition dipole moments integrals
 tdm = np.load(path_tdm) # Load transition dipole moment matrix in AO basis 
-#mol = change_mol_units(readXYZ(mol_file)) # Load molecule in Angstroms and convert it in atomic units
+
 n_atoms, n_frames = get_numberofatoms(mol_file) 
 atoms = read_atomlist(mol_file, n_atoms)
 nocc = 50 # Number of occupied orbitals
 nvirt = c_ao.shape[1] - nocc # Number of virtual orbitals 
 
-# Create a dictionary to assign the number of spherical functions for each atom. We will need it later in several places.  
-#dictCGFs = create_dict_CGFs(path_hdf5, basisname, mol)
-#cgfs_per_atoms = {s: len(dictCGFs[s]) for s in dictCGFs.keys()}
-# There should be a better way to know the number of spherical functions. Now hard-coded. 
+### Make a function to compute the number of spherical functions for each atom 
+xs = [f5['cp2k/basis/{}/{}/coefficients'.format(atoms[i], basisname)] for i in range(n_atoms)]
+ys = [calc_orbital_Slabels(package_name, read_basis_format(package_name, xs[i].attrs['basisFormat'])) for i in range(n_atoms)] 
+n_sph_atoms = np.stack(np.sum(len(x) for x in ys[i]) for i in range(n_atoms)) 
 
-# Now compute the charge transition densities using the Lowdin formula  
-# First transform the c_ao in MO basis
+### Make a function tha returns in transition density charges 
+# First transform the c_ao in MO basis using lowdin 
 sqrt_s = sqrtm(s)
 c_mo = np.dot(sqrt_s, c_ao)
 q = np.zeros((n_atoms, c_mo.shape[1], c_mo.shape[1])) # Size of the transition density tensor : n_atoms x n_mos x n_mos 
 
 index = 0 
 for i in range(n_atoms):
-    q[i, :, :] = np.dot(c_mo[index:(index + n_sph(atoms[i])), :].T, c_mo[index:(index + n_sph(atoms[i])), :])
-    index += n_sph(atoms[i])
+    q[i, :, :] = np.dot(c_mo[index:(index + n_sph_atoms[i]), :].T, c_mo[index:(index + n_sph_atoms[i])), :])
+    index += n_sph_atoms[i])
 
-# Compute the Mataga-Nishimoto-Ohno_Klopman damped Columb and Excgange law functions 
+### Make a function that compute the Mataga-Nishimoto-Ohno_Klopman damped Columb and Excgange law functions 
 coords = read_xyz_coordinates(mol_file, n_atoms, 1)
 coords = coords * 1.8897259886 # Conversion from Angstroms to atomic units
 r_ab = cdist(coords, coords) # Distance matrix between atoms A and B 
 hardness_vec = np.stack(hardness(atoms[i]) for i in range(n_atoms)).reshape(n_atoms, 1)
 hard = np.dot(hardness_vec, hardness_vec.T) / 2 
-
 beta = beta1 + ax * beta2
 alpha = alpha1 + ax * alpha2 
-
 gamma_J = np.power(1 / (np.power(r_ab, beta) + ax * np.power(hard, -beta)), 1/beta)
 gamma_J[gamma_J == np.inf] = 0 # When ax = 0 , you can get infinite values on the diagonal. Just turn them off to 0. 
 gamma_K = np.power(1 / (np.power(r_ab, alpha) + np.power(hard, -alpha)), 1/alpha)
@@ -146,13 +146,13 @@ weight = np.hstack(np.max(cis[:, i] ** 2) for i in range(nocc*nvirt)) # weight o
 index_weight = np.hstack(np.where( cis[:, i] ** 2 == np.max(cis[:, i] ** 2) ) for i in range(nocc*nvirt)).reshape(nocc*nvirt) # Find the index of this transition
 index_i = np.stack(excs[index_weight[i]][0] for i in range(nocc*nvirt)) # Index of the hole
 index_a = np.stack(excs[index_weight[i]][1] for i in range(nocc*nvirt))
-e_orb_i = e[index_i] * 27.211 # These are the energies of the hole for the transition with the larger weight
-e_orb_a = e[index_a] * 27.211  # These are the energies of the electron for the transition with the larger weight
+e_orb_i = e[index_i] * h2ev # These are the energies of the hole for the transition with the larger weight
+e_orb_a = e[index_a] * h2ev  # These are the energies of the electron for the transition with the larger weight
 e_singorb = (e_orb_a - e_orb_i )  # This is the energy for the transition with the larger weight 
 
 output = np.empty((nocc*nvirt, 12))
 output[:, 0] = 0 # State number: we update it after reorder
-output[:, 1] = omega * 27.211 # State energy in eV
+output[:, 1] = omega * h2ev # State energy in eV
 output[:, 2] = f # Oscillator strength
 output[:, 3] = d_x # Transition dipole moment in the x direction
 output[:, 4] = d_y # Transition dipole moment in the y direction
@@ -168,9 +168,6 @@ output = output[output[:, 1].argsort()] # Reorder the output in ascending order 
 output[:, 0] = np.arange(nocc * nvirt) # Give a state number in the correct order
 
 np.savetxt('output.txt', output, fmt='%5d %10.3f %10.5f %10.5f %10.5f %10.5f %10.5f %3d %10.3f %3d %10.3f %10.3f')
-
-
-
 
 def get_numberofatoms(fn): 
     # Retrieve number of lines in trajectory file
@@ -192,7 +189,6 @@ def read_atomlist(fn, n_atoms):
     atoms = pd.read_csv(fn, nrows = n_atoms, delim_whitespace=True, header=None, 
                         skiprows = 2, usecols=[0]).astype(str)
     return atoms[0][:]      
-
 
 def read_xyz_coordinates(fn, n_atoms, iframe): 
     # Read xyz coordinate from a (trajectory) xyz file. 
