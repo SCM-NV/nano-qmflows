Fixed bug on computation of hardness in stddft

nac/workflows/workflow_stddft_spectrum.py
@@ -272,10 +272,10 @@ def compute_MNOK_integrals(mol, xc_dft):
     # Distance matrix between atoms A and B
     r_ab = cdist(coords, coords)
     hardness_vec = np.stack(hardness(mol[i][0]) for i in range(n_atoms)).reshape(n_atoms, 1)
-    hard = np.dot(hardness_vec, hardness_vec.T) / 2
+    hard = np.add(hardness_vec, hardness_vec.T) / 2
     beta = xc(xc_dft)['beta1'] + xc(xc_dft)['ax'] * xc(xc_dft)['beta2']
     alpha = xc(xc_dft)['alpha1'] + xc(xc_dft)['ax'] * xc(xc_dft)['alpha2']
-    gamma_J = np.power(1 / (np.power(r_ab, beta) + xc(xc_dft)['ax'] * np.power(hard, -beta)), 1/beta)
+    gamma_J = np.power(1 / (np.power(r_ab, beta) + np.power( ( xc(xc_dft)['ax'] * hard ), -beta)), 1/beta)
     # When ax = 0 , you can get infinite values on the diagonal. Just turn them off to 0.
     gamma_J[gamma_J == np.inf] = 0
     gamma_K = np.power(1 / (np.power(r_ab, alpha) + np.power(hard, -alpha)), 1/alpha)