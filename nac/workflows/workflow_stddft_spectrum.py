64675ed2fbc8237dca36d044d228bd5374b3ade2
Ivan Infante <iinfant76@gmail.com>
2018-09-10 11:13:57 +0200

    Added a better output format. Added also excitonic descriptor, but is off now

--- nac/workflows/workflow_stddft_spectrum.py
+++ nac/workflows/workflow_stddft_spectrum.py
@@ -65,52 +65,60 @@ def compute_excited_states_tddft(
     # Number of virtual orbitals
     nvirt = c_ao.shape[1] - nocc
 
-    # Call the function that computes overlaps
-    print("Reading or computing the overlap matrix")
-    s = get_multipole_matrix(
-        i, mol, config, 'overlap')
+    if tddft == 'sing_orbs': 
+       omega = -np.subtract(e[:nocc].reshape(nocc, 1), e[nocc:].reshape(nvirt, 1).T).reshape(nocc*nvirt)  
+       xia = np.eye(nocc*nvirt)  
+    else:  
+       # Call the function that computes overlaps
+       print("Reading or computing the overlap matrix")
+       s = get_multipole_matrix(
+           i, mol, config, 'overlap')
 
-    # Make a function tha returns in transition density charges
-    print("Computing the transition density charges") 
-    q = transition_density_charges(mol, config, s, c_ao)
+       # Make a function tha returns in transition density charges
+       print("Computing the transition density charges") 
+       q = transition_density_charges(mol, config, s, c_ao)
 
-    # Make a function that compute the Mataga-Nishimoto-Ohno_Klopman damped Columb and Excgange law functions
-    print("Computing the gamma functions for Exchange and Coulomb integrals") 
-    gamma_J, gamma_K = compute_MNOK_integrals(mol, xc_dft)
+       # Make a function that compute the Mataga-Nishimoto-Ohno_Klopman damped Columb and Excgange law functions
+       print("Computing the gamma functions for Exchange and Coulomb integrals") 
+       gamma_J, gamma_K = compute_MNOK_integrals(mol, xc_dft)
 
-    # Compute the Couloumb and Exchange integrals
-    # If xc_dft is a pure functional, ax=0, thus the pqrs_J ints are not needed and can be set to 0.  
-    print("Computing the Exchange and Coulomb integrals") 
-    if (xc(xc_dft)['type'] == 'pure'):
-        pqrs_J = np.zeros((e.size, e.size, e.size, e.size))
-    else:
-        pqrs_J = np.tensordot(q, np.tensordot(q, gamma_J, axes=(0, 1)), axes=(0, 2))
-    pqrs_K = np.tensordot(q, np.tensordot(q, gamma_K, axes=(0, 1)), axes=(0, 2))
+       # Compute the Couloumb and Exchange integrals
+       # If xc_dft is a pure functional, ax=0, thus the pqrs_J ints are not needed and can be set to 0.  
+       print("Computing the Exchange and Coulomb integrals") 
+       if (xc(xc_dft)['type'] == 'pure'):
+           pqrs_J = np.zeros((e.size, e.size, e.size, e.size))
+       else:
+           pqrs_J = np.tensordot(q, np.tensordot(q, gamma_J, axes=(0, 1)), axes=(0, 2))
+       pqrs_K = np.tensordot(q, np.tensordot(q, gamma_K, axes=(0, 1)), axes=(0, 2))
 
-    # Construct the Tamm-Dancoff matrix A for each pair of i->a transition
-    print("Constructing the A matrix for TDDFT calculation")
-    a_mat = construct_A_matrix_tddft(pqrs_J, pqrs_K, nocc, nvirt, xc_dft, e)
+       # Construct the Tamm-Dancoff matrix A for each pair of i->a transition
+       print("Constructing the A matrix for TDDFT calculation")
+       a_mat = construct_A_matrix_tddft(pqrs_J, pqrs_K, nocc, nvirt, xc_dft, e)
 
-    if tddft == 'stddft':
-        print('sTDDFT has not been implemented yet !')
-    # Solve the eigenvalue problem = A * cis = omega * cis
-    elif tddft == 'stda':
-        print("This is a TDA calculation ! \n Solving the eigenvalue problem")
-        omega, xia = np.linalg.eig(a_mat)
-    else:
-        msg = "Only the stda method is available"
-        raise RuntimeError(msg)
+       if tddft == 'stddft':
+           print('sTDDFT has not been implemented yet !')
+           # Solve the eigenvalue problem = A * cis = omega * cis
+       elif tddft == 'stda':
+           print("This is a TDA calculation ! \n Solving the eigenvalue problem")
+           omega, xia = np.linalg.eig(a_mat)
+       else:
+           msg = "Only the stda method is available"
+           raise RuntimeError(msg)
 
     # Compute oscillator strengths
-    print("Computing the oscillator strengths") 
     # The formula can be rearranged like this:
     # f_I = 2/3 * np.sqrt(2 * omega_I) * sum_ia ( np.sqrt(e_diff_ia) * xia * tdm_x) ** 2 + y^2 + z^2
+
     # 1) Get the energy matrix i->a. Size: Nocc * Nvirt
     delta_ia = -np.subtract(e[:nocc].reshape(nocc, 1), e[nocc:].reshape(nvirt, 1).T).reshape(nocc*nvirt)
 
     # 2) Compute the transition dipole matrix TDM(i->a)
     # Call the function that computes transition dipole moments integrals
+    print("Reading or computing the transition dipole matrix")
     tdm = get_multipole_matrix(i, mol, config, 'dipole')
+    print("Reading or computing the transition quadrupole matrix")
+    tqm = get_multipole_matrix(
+         i, mol, config, 'quadrupole')
     tdmatrix_x = np.linalg.multi_dot([c_ao[:, :nocc].T, tdm[0, :, :], c_ao[:, nocc:]]).reshape(nocc*nvirt)
     tdmatrix_y = np.linalg.multi_dot([c_ao[:, :nocc].T, tdm[1, :, :], c_ao[:, nocc:]]).reshape(nocc*nvirt)
     tdmatrix_z = np.linalg.multi_dot([c_ao[:, :nocc].T, tdm[2, :, :], c_ao[:, nocc:]]).reshape(nocc*nvirt)
@@ -125,11 +133,68 @@ def compute_excited_states_tddft(
 
     # Write to output
     output = write_output_tddft(nocc, nvirt, omega, f, d_x, d_y, d_z, xia, e)
-    path_output = os.path.join(config['work_dir'], 'output_{}.txt'.format(i))
-    np.savetxt(path_output, output, fmt='%5d %10.3f %10.5f %10.5f %10.5f %10.5f %10.5f %3d %10.3f %3d %10.3f %10.3f')
+    path_output = os.path.join(config['work_dir'], 'output_{}_{}.txt'.format(i, tddft))
+    header = '{:^5s}{:^14s}{:^8s}{:^11s}{:^11s}{:^11s}{:^11s}{:<5s}{:^10s}{:<5s}{:^11s}{:^11s}'.format(
+             'state', 'energy', 'f', 't_dip_x', 't_dip_y', 't_dip_y', 'weight', 'from', 'energy', 'to', 'energy', 'delta_E') 
+    np.savetxt(path_output, output, fmt='%5d %10.3f %10.5f %10.5f %10.5f %10.5f %10.5f %3d %10.3f %3d %10.3f %10.3f', header=header)
+
+    descriptors = False   
+    if descriptors:
+       xia_I = xia.reshape(nocc, nvirt, nocc*nvirt)
+       # Find the index of the n_lowest excitations
+       idx = np.argsort(omega[:n_lowest])
+       # Generate NTOs for each excited state
+       # Solve the SVD for each state and store it into a list
+       xs = []
+       for i in range(n_lowest):
+           xs.append(np.linalg.svd(xia_I[:, :, idx[i] ]))
+       # Do some renaming it for clearness
+       v_ip_I = np.array([xs[i][0] for i in range(n_lowest)])
+       lamb_p_I = np.array([xs[i][1] for i in range(n_lowest)])
+       w_ip_I = np.array([xs[i][2] for i in range(n_lowest)])
+       # Compute the NTO participation ratio 
+       pr_nto = np.stack( ( np.sum(lamb_p_I[i, :] ** 2) ) ** 2 / np.sum(lamb_p_I[i, :] ** 4) for i in range(n_lowest) )
+
+       # Transform the transition density matrix into AO basis 
+       d0I_ao = np.stack(np.linalg.multi_dot([c_ao[:, :nocc], xia_I[:, :, i], c_ao[:, nocc:].T]) for i in range(n_lowest))
+       om = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, s, d0I_ao[i, :, :], s])) for i in range(n_lowest))
+       
+       x_h = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, tdm[0, :, :], d0I_ao[i, :, :], s])) for i in range(n_lowest)) 
+       y_h = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, tdm[1, :, :], d0I_ao[i, :, :], s])) for i in range(n_lowest)) 
+       z_h = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, tdm[2, :, :], d0I_ao[i, :, :], s])) for i in range(n_lowest))
+
+       x_e = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, s, d0I_ao[i, :, :],  tdm[0, :, :]])) for i in range(n_lowest)) 
+       y_e = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, s, d0I_ao[i, :, :],  tdm[1, :, :]])) for i in range(n_lowest)) 
+       z_e = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, s, d0I_ao[i, :, :],  tdm[2, :, :]])) for i in range(n_lowest))
+
+       print("Reading or computing the overlap matrix")
+       tqm = get_multipole_matrix(
+           i, mol, config, 'quadrupole')
+
+       x2_h = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, tqm[0, :, :], d0I_ao[i, :, :], s])) for i in range(n_lowest)) 
+       y2_h = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, tqm[1, :, :], d0I_ao[i, :, :], s])) for i in range(n_lowest)) 
+       z2_h = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, tqm[2, :, :], d0I_ao[i, :, :], s])) for i in range(n_lowest))
+
+       x2_e = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, s, d0I_ao[i, :, :],  tqm[0, :, :]])) for i in range(n_lowest)) 
+       y2_e = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, s, d0I_ao[i, :, :],  tqm[1, :, :]])) for i in range(n_lowest)) 
+       z2_e = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, s, d0I_ao[i, :, :],  tqm[2, :, :]])) for i in range(n_lowest))
+
+       x_h_x_e = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, tdm[0, :, :], d0I_ao[i, :, :], tdm[0, :, :]])) for i in range(n_lowest))
+       y_h_y_e = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, tdm[1, :, :], d0I_ao[i, :, :], tdm[1, :, :]])) for i in range(n_lowest))
+       z_h_z_e = np.stack(np.trace(np.linalg.multi_dot([d0I_ao[i, :, :].T, tdm[2, :, :], d0I_ao[i, :, :], tdm[2, :, :]])) for i in range(n_lowest))
+
+       d_exc = np.sqrt( ( (x2_h - 2 * x_h_x_e + x2_e) + (y2_h - 2 * y_h_y_e + y2_e) + (z2_h - 2 * z_h_z_e + z2_e) ) / om )
+       d_h_e = np.abs(( ( x_e - x_h) + (y_e - y_h) + (z_e - z_h) ) / om)
+
+       sigma_h = np.sqrt( (x2_h / om - (x_h / om ) ** 2) + (y2_h / om - (y_h / om ) ** 2) + (z2_h / om - (z_h / om ) ** 2) )
+       sigma_e = np.sqrt( (x2_e / om - (x_e / om ) ** 2) + (y2_e / om - (y_e / om ) ** 2) + (z2_e / om - (z_e / om ) ** 2) )
+
+       cov = ( x_h_x_e - x_h * x_e ) + ( y_h_y_e - y_h * y_e ) + ( z_h_z_e - z_h * z_e )
+       r_e_h = cov / (sigma_h * sigma_e) 
+
+       
 
     return path_output
-
 
 def write_output_tddft(nocc, nvirt, omega, f, d_x, d_y, d_z, xia, e):
     """ Write out as a table in plane text"""
