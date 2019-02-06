982c5438cc56a2ea05bbbb4889cd978d371ea9f2
felipe <tifonzafel@gmail.com>
2019-02-06 16:47:27 +0100

    clean namespace

--- nac/__init__.py
+++ nac/__init__.py
@@ -14,15 +14,15 @@ from .integrals import (
     calculate_couplings_levine, general_multipole_matrix)
 
 from .schedule import (
-    calculate_mos, create_point_folder, lazy_couplings, prepare_cp2k_settings,
-    prepare_job_cp2k, split_file_geometries, write_hamiltonians)
+    calculate_mos, lazy_couplings, prepare_cp2k_settings,
+    prepare_job_cp2k, write_hamiltonians)
 
 from .analysis import (
     autocorrelate, dephasing, gauss_function, parse_list_of_lists,
     read_couplings, read_energies, rdf, spectral_density)
 
 from .workflows import (
-    initialize, store_transf_matrix,  workflow_derivative_couplings)
+    workflow_derivative_couplings, workflow_stddft)
 
 
 __all__ = ['Array', 'AtomBasisData', 'AtomBasisKey', 'AtomData', 'AtomXYZ',
@@ -31,12 +31,12 @@ __all__ = ['Array', 'AtomBasisData', 'AtomBasisKey', 'AtomData', 'AtomXYZ',
            'calcMtxMultipoleP', 'calcMtxOverlapP',
            'calc_transf_matrix', 'calculate_couplings_levine', 'calculate_mos',
            'change_mol_units', 'create_normalized_CGFs', 'createUniqueCGF', 'create_dict_CGFs',
-           'create_point_folder', 'dephasing', 'expandBasisOneCGF',
+           'dephasing', 'expandBasisOneCGF',
            'expandBasis_cp2k', 'expandBasis_turbomole', 'fs_to_cm', 'fs_to_nm',
            'gauss_function', 'general_multipole_matrix',
-           'getmass', 'hbar', 'hardness', 'initialize', 'lazy_couplings',
+           'getmass', 'hbar', 'hardness', 'lazy_couplings',
            'parse_list_of_lists', 'prepare_cp2k_settings', 'prepare_job_cp2k', 'r2meV',
            'read_couplings', 'read_energies', 'retrieve_hdf5_data', 'rdf',
-           'search_data_in_hdf5', 'spectral_density', 'split_file_geometries',
-           'store_arrays_in_hdf5', 'store_transf_matrix', 'triang2mtx',
-           'workflow_derivative_couplings', 'write_hamiltonians']
+           'search_data_in_hdf5', 'spectral_density',
+           'store_arrays_in_hdf5', 'triang2mtx',
+           'workflow_derivative_couplings', 'workflow_stddft', 'write_hamiltonians']
