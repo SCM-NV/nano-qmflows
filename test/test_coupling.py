372032be9a0ae3eb568c3c7195bd3d7296995a33
felipe <tifonzafel@gmail.com>
2019-02-07 10:36:06 +0100

    removed unused tests files

--- test/test_coupling.py
+++ test/test_coupling.py
@@ -24,19 +24,19 @@ project_name = 'Cd33Se33'
 input_file = join(root, 'test/test_files/input_test_derivative_couplings.yml')
 
 
-@pytest.mark.slow
-def test_fast_couplings(tmp_path):
-    """
-    Check that the couplings run
-    """
-    file_path = join(root, 'test/test_files/input_fast_test_derivative_couplings.yml')
-    config = process_input(file_path, 'derivative_couplings')
-    # config["scratch_path"] = tmp_path
-    try:
-        workflow_derivative_couplings(config)
-    except:
-        print("Unexpected error:", sys.exc_info()[0])
-        raise
+# @pytest.mark.slow
+# def test_fast_couplings(tmp_path):
+#     """
+#     Check that the couplings run
+#     """
+#     file_path = join(root, 'test/test_files/input_fast_test_derivative_couplings.yml')
+#     config = process_input(file_path, 'derivative_couplings')
+#     # config["scratch_path"] = tmp_path
+#     try:
+#         workflow_derivative_couplings(config)
+#     except:
+#         print("Unexpected error:", sys.exc_info()[0])
+#         raise
 
 
 @pytest.mark.slow
