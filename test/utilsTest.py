ced7b7755480d5e21255cc101a578ce0cb84590f
felipe <tifonzafel@gmail.com>
2019-03-03 12:09:54 +0100

    removed unused test code

--- test/utilsTest.py
+++ test/utilsTest.py
@@ -1,62 +1,5 @@
-
-from functools import wraps
-from nac.common import InputKey
 from os.path import join
-
 import h5py
-import numpy as np
-import os
-import shutil
-
-# ========================<>=======================
-angs2au = 1 / 0.529177249
-
-
-def remove_file_directory(paths):
-    """
-    Remove both a file or a directory
-    """
-    for p in paths:
-        if os.path.exists(p):
-            if os.path.isfile(p):
-                os.remove(p)
-            else:
-                shutil.rmtree(p)
-
-
-def try_to_remove(path_files=None):
-    """
-    Decorator to remove the intermediate data created during the testing.
-    """
-    if path_files is None:
-        path_files = ['test/test_files/test.hdf5']
-
-    def remove_test_files(fun):
-        @wraps(fun)
-        def wrapper(*args, **kwargs):
-            try:
-                return fun(*args, **kwargs)
-            finally:
-                remove_file_directory(path_files)
-        return wrapper
-    return remove_test_files
-
-
-def dump_MOs_coeff(handle_hdf5, packageHDF5, path_MO, pathEs, pathCs,
-                   nOrbitals):
-    """
-    MO coefficients are stored in row-major order, they must be transposed
-    to get the standard MO matrix.
-    :param files: Files to calculate the MO coefficients
-    :type  files: Namedtuple (fileXYZ,fileInput,fileOutput)
-    :param job: Output File
-    :type  job: String
-    """
-    key = InputKey('orbitals', [path_MO, nOrbitals, pathEs, pathCs])
-
-    packageHDF5(handle_hdf5, [key])
-
-    return pathEs, pathCs
 
 
 def copy_basis_and_orbitals(source, dest, project_name):
@@ -73,22 +16,3 @@ def copy_basis_and_orbitals(source, dest, project_name):
                 if not any(x in l for x in excluded):
                     path = join(k, l)
                     f5.copy(path, g5[k])
-
-
-def fromIndex(ixs, shape):
-    i, j = ixs
-    if j >= i:
-        k = sum(m * k for m, k in zip(shape[1:], ixs)) + ixs[-1]
-        r = (((i * i + i) // 2) if i else 0)
-        return k - r
-    else:
-        return fromIndex([j, i], shape)
-
-
-def triang2mtx(arr, dim):
-    rss = np.empty((dim, dim))
-    for i in range(dim):
-        for j in range(dim):
-            k = fromIndex([i, j], [dim, dim])
-            rss[i, j] = arr[k]
-    return rss
