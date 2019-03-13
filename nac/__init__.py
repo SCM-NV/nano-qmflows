931f27ab025c789f5b105e0d1d3d404a12a707b1
felipe <tifonzafel@gmail.com>
2019-03-13 11:43:18 +0100

    added analysis function to __init__

--- nac/__init__.py
+++ nac/__init__.py
@@ -1,3 +1,9 @@
+from .analysis import (
+    autocorrelate, dephasing, convolute, func_conv, gauss_function,
+    parse_list_of_lists, read_couplings, read_energies,
+    read_energies_pyxaid, read_pops_pyxaid, rdf, spectral_density
+)
+
 from .integrals import (
     calculate_couplings_levine, compute_overlaps_for_coupling)
 
@@ -6,8 +12,10 @@ from .schedule import (calculate_mos, lazy_couplings)
 from .workflows import (
     workflow_derivative_couplings, workflow_stddft)
 
-
 __all__ = [
-    'calculate_couplings_levine', 'calculate_mos',
-    'compute_overlaps_for_coupling', 'lazy_couplings',
+    'autocorrelate', 'calculate_couplings_levine', 'calculate_mos',
+    'compute_overlaps_for_coupling', 'convolute', 'dephasing',
+    'func_conv', 'gauss_function', 'lazy_couplings',
+    'parse_list_of_lists', 'read_couplings', 'read_energies',
+    'read_energies_pyxaid', 'read_pops_pyxaid', 'rdf', 'spectral_density',
     'workflow_derivative_couplings', 'workflow_stddft']
