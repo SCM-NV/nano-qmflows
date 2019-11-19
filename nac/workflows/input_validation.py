c05b95e1574e28881d4cde2319c169c1b8287086
felipez <tifonzafel@gmail.com>
2019-11-19 14:12:27 +0100

    fixed comment

--- nac/workflows/input_validation.py
+++ nac/workflows/input_validation.py
@@ -263,7 +263,7 @@ def compute_HOMO_index(path_traj_xyz: str, basis: str, charge: int) -> int:
     number_of_electrons = sum(
         valence_electrons['-'.join((at.symbol, basis))] for at in mol.atoms)
 
-    """ Correct for total charge of the system """
+    # Correct for total charge of the system
     number_of_electrons = number_of_electrons - charge
 
     if (number_of_electrons % 2) != 0:
