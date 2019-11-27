052c839afc51e7e3e306ae73579c1966252123e7
felipez <tifonzafel@gmail.com>
2019-11-27 17:15:44 +0100

    removed files after testing
    Signed-off-by: felipez <tifonzafel@gmail.com>

--- test/test_workflow_IPR.py
+++ test/test_workflow_IPR.py
@@ -14,10 +14,9 @@ def test_workflow_IPR(tmp_path):
     """Test the Inverse Participation Ratio workflow."""
     file_path = join(root, 'test/test_files/input_test_IPR.yml')
     config = process_input(file_path, 'ipr_calculation')
-    print("config: ", config)
     try:
-        result = workflow_ipr(config)
-        print("IPR", result)
+        workflow_ipr(config)
+        os.remove("IPR.txt")
     except BaseException:
         print("scratch_path: ", tmp_path)
         print("Unexpected error:", sys.exc_info()[0])
--- test/test_workflow_coop.py
+++ test/test_workflow_coop.py
@@ -17,9 +17,9 @@ def test_workflow_coop(tmp_path):
     """Test the Crystal Orbital Overlap Population workflow."""
     file_path = join(root, 'test/test_files/input_test_coop.yml')
     config = process_input(file_path, 'coop_calculation')
-    print("config: ", config)
     try:
         workflow_crystal_orbital_overlap_population(config)
+        os.remove("COOP.txt")
     except BaseException:
         print("scratch_path: ", tmp_path)
         print("Unexpected error:", sys.exc_info()[0])
