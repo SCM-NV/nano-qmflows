bb0520376eaba12270ee851ec92a272567644ec2
felipez <tifonzafel@gmail.com>
2020-03-24 11:06:41 +0100

    added typing for multipole_matrices

--- nac/integrals/multipole_matrices.py
+++ nac/integrals/multipole_matrices.py
@@ -1,19 +1,57 @@
-from compute_integrals import compute_integrals_multipole
-from nac.common import (
-    Matrix, retrieve_hdf5_data, is_data_in_hdf5,
-    store_arrays_in_hdf5, tuplesXYZ_to_plams)
-from os.path import join
+"""Compute multipole integrals using `Libint2 <https://github.com/evaleev/libint/wiki>`.
+
+The interface to the C++ Libint2 library is located at the parent folder,
+in the `libint` folder.
+Index
+-----
+.. currentmodule:: nac.integrals.multipole_matrices
+.. autosummary::
+    get_multipole_matrix
+    compute_matrix_multipole
+API
+---
+.. autofunction:: get_multipole_matrix
+.. autofunction:: compute_matrix_multipole
+"""
+import logging
 import os
 import uuid
+from os.path import join
+from pathlib import Path
+from typing import List, Optional
+import numpy as np
+
+from compute_integrals import compute_integrals_multipole
+from qmflows.common import AtomXYZ
+from qmflows.type_hints import PathLike
+
+from ..common import (DictConfig, Matrix, is_data_in_hdf5, retrieve_hdf5_data,
+                      store_arrays_in_hdf5, tuplesXYZ_to_plams)
+
+logger = logging.getLogger(__name__)
 
 
-def get_multipole_matrix(config: dict, inp: dict, multipole: str) -> Matrix:
+def get_multipole_matrix(config: DictConfig, inp: dict, multipole: str) -> Matrix:
+    """Retrieve the `multipole` number `i` from the trajectory. Otherwise compute it.
+
+    Parameters
+    ----------
+    config
+        Global configuration to run a workflow
+    inp
+        Information about the current point, e.g. molecular geometry.
+    multipole
+        Either overlap, dipole or quadrupole.
+
+    Returns
+    -------
+    np.ndarray
+        Tensor containing the multipole.
+
     """
-    Retrieve the `multipole` number `i` from the trajectory. Otherwise compute it.
-    """
-    root = join(config['project_name'], 'multipole',
+    root = join(config.project_name, 'multipole',
                 f'point_{inp.i + config.enumerate_from}')
-    path_hdf5 = config['path_hdf5']
+    path_hdf5 = config.path_hdf5
     path_multipole_hdf5 = join(root, multipole)
     matrix_multipole = search_multipole_in_hdf5(
         path_hdf5, path_multipole_hdf5, multipole)
@@ -25,33 +63,45 @@ def get_multipole_matrix(config: dict, inp: dict, multipole: str) -> Matrix:
     return matrix_multipole
 
 
-def search_multipole_in_hdf5(path_hdf5: str, path_multipole_hdf5: str, multipole: str):
-    """
-    Search if the multipole is already store in the HDFt
-    """
+def search_multipole_in_hdf5(
+        path_hdf5: PathLike, path_multipole_hdf5: str, multipole: str) -> Optional[np.ndarray]:
+    """Search if the multipole is already store in the HDF5."""
     if is_data_in_hdf5(path_hdf5, path_multipole_hdf5):
-        print(f"retrieving multipole: {multipole} from the hdf5")
+        logger.info(f"retrieving multipole: {multipole} from the hdf5")
         return retrieve_hdf5_data(path_hdf5, path_multipole_hdf5)
     else:
-        print(f"computing multipole: {multipole}")
+        logger.info(f"computing multipole: {multipole}")
         return None
 
 
 def compute_matrix_multipole(
-        mol: list, config: dict, multipole: str) -> Matrix:
-    """
-    Compute the some `multipole` matrix: overlap, dipole, etc. for a given geometry `mol`.
-    Compute the Multipole matrix in spherical coordinates.
+        mol: List[AtomXYZ], config: DictConfig, multipole: str) -> Matrix:
+    """Compute a `multipole` matrix: overlap, dipole, etc. for a given geometry `mol`.
 
-    Note: for the dipole onwards the super_matrix contains all the matrices stack all the
+    The multipole is Computed in spherical coordinates.
+
+    Note: for the dipole and quadrupole the super_matrix contains all the matrices stack all the
     0-axis.
 
-    :returns: Matrix with entries <ψi | x^i y^j z^k | ψj>
+    Parameters
+    ----------
+    mol
+        Molecule to compute the dipole
+    config
+        Dictionary with the current configuration
+    multipole
+        kind of multipole to compute
+
+    Returns
+    -------
+    np.ndarray
+        Matrix with entries <ψi | x^i y^j z^k | ψj>
+
     """
-    path_hdf5 = config['path_hdf5']
+    path_hdf5 = config.path_hdf5
 
     # Write molecule in temporal file
-    path = join(config["scratch_path"], f"molecule_{uuid.uuid4()}.xyz")
+    path = join(config.scratch_path, f"molecule_{uuid.uuid4()}.xyz")
     mol_plams = tuplesXYZ_to_plams(mol)
     mol_plams.write(path)
 
