6d47cefcc18a67fc66c84796ef2eb51c2ec8eaaa
felipe zapata <tifonzafel@gmail.com>
2020-03-28 16:57:51 +0100

    fixed mac compilation error

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ class BuildExt(build_ext):
     }
 
     if sys.platform == 'darwin':
-        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7', '-fno-aligned-allocation', '-fno-sized-deallocation']
         c_opts['unix'] += darwin_opts
         l_opts['unix'] += darwin_opts
 
