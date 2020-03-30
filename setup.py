0b2f6177035ad1e474df944374b22ee59daef4f9
felipe zapata <tifonzafel@gmail.com>
2020-03-30 09:56:59 +0200

    use -fno-size-deallocation

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ class BuildExt(build_ext):
     }
 
     if sys.platform == 'darwin':
-        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7', '-fsized-deallocation']
+        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7', '-fno-sized-deallocation']
         c_opts['unix'] += darwin_opts
         l_opts['unix'] += darwin_opts
 
