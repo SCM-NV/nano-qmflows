9e120a124fad7414da9469433cd4f9a3ab8589f3
felipez <tifonzafel@gmail.com>
2020-06-22 13:10:48 +0200

    allows scratch path to be None:

--- test/test_initialization.py
+++ test/test_initialization.py
@@ -1,26 +1,35 @@
 """Test that the path are created propery."""
 
 from pathlib import Path
-
+from typing import Optional
 import yaml
 
 from nanoqm.workflows.initialization import initialize
 from nanoqm.workflows.input_validation import process_input
+from qmflows.type_hints import PathLike
 
 from .utilsTest import PATH_TEST
 
 
-def test_run_workflow(tmp_path):
+def test_run_workflow(tmp_path: PathLike) -> None:
     """Check that all the paths are initialize."""
+    create_config(tmp_path, True)
+    create_config(tmp_path, False)
+
+
+def create_config(tmp_path: str, scrath_is_None: bool) -> str:
     path = PATH_TEST / "input_fast_test_derivative_couplings.yml"
     with open(path, 'r') as f:
         inp = yaml.load(f, yaml.FullLoader)
 
     # change scratch
-    inp["scratch_path"] = (tmp_path / "level0" / "level1").as_posix()
-
-    # change HDF5 path
-    inp["path_hdf5"] = (Path(inp["scratch_path"]) / "test_init.hdf5").as_posix()
+    if scrath_is_None:
+        inp["scratch_path"] = None
+        del inp["path_hdf5"]
+    else:
+        inp["scratch_path"] = (tmp_path / "level0" / "level1").as_posix()
+        # change HDF5 path
+        inp["path_hdf5"] = (Path(inp["scratch_path"]) / "test_init.hdf5").as_posix()
 
     path_inp = tmp_path / "test_init.yml"
     with open(path_inp, 'w') as f:
