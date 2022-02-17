03e3426cf21d7a0bbb8570de015a1a8c093fdecf
Bas van Beek <b.f.van.beek@vu.nl>
2022-02-17 18:36:40 +0100

    Update install_cpp_deps.sh

--- tools/install_cpp_deps.sh
+++ tools/install_cpp_deps.sh
@@ -3,7 +3,7 @@
 
 PYTHON_VERSION="$1"
 
-wget -nv https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
 chmod u+rx miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 $HOME/miniconda/bin/conda install -c conda-forge python=$PYTHON_VERSION boost eigen libint==2.6.0 highfive -y
