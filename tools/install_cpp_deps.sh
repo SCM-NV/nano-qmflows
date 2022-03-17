3a4a63d5e66c449eef1c044ccdb2293e7e2bb452
Bas van Beek <b.f.van.beek@vu.nl>
2022-03-17 11:45:57 +0100

    f

--- tools/install_cpp_deps.sh
+++ tools/install_cpp_deps.sh
@@ -3,50 +3,57 @@
 set -e
 
 setup_boost () {
+    start=$SECONDS
+
     echo ::group::"Download boost"
-    curl -Ls https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz -o boost_1_78_0.tar.gz
+    curl -L https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz -o boost_1_78_0.tar.gz
     tar -xf boost_1_78_0.tar.gz
     mv boost_1_78_0 boost
     export BOOST_INCLUDEDIR="$PWD/boost/boost"
     echo ::endgroup::
+
+    printf "%70.70s\n" "✓ $(($SECONDS - $start))s"
 }
 
 setup_eigen () {
+    start=$SECONDS
+
     echo ::group::"Download eigen"
-    curl -s https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz -o eigen-3.4.0.tar.gz
+    curl https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz -o eigen-3.4.0.tar.gz
     tar -xf eigen-3.4.0.tar.gz
     mv eigen-3.4.0 eigen
     export EIGEN3_INCLUDEDIR="$PWD/eigen/Eigen"
     echo ::endgroup::
+
+    printf "%70.70s\n" "✓ $(($SECONDS - $start))s"
 }
 
 setup_libint () {
+    start=$SECONDS
+
     echo ::group::"Download libint"
-    curl -Ls https://github.com/evaleev/libint/archive/refs/tags/v2.6.0.tar.gz -o libint-2.6.0.tar.gz
+    curl -L https://github.com/evaleev/libint/archive/refs/tags/v2.6.0.tar.gz -o libint-2.6.0.tar.gz
     tar -xf libint-2.6.0.tar.gz
     mv libint-2.6.0 libint
     export LIBINT_INCLUDEDIR="$PWD/libint/include"
     echo ::endgroup::
-}
 
-setup_highfive () {
-    echo ::group::"Download highfive"
-    curl -Ls https://github.com/BlueBrain/HighFive/archive/refs/tags/v2.3.1.tar.gz -o highfive-2.3.1.tar.gz
-    tar -xf highfive-2.3.1.tar.gz
-    mv highfive-2.3.1 highfive
-    export HIGHFIVE_INCLUDEDIR="$PWD/highfive/include"
-    echo ::endgroup::
+    printf "%70.70s\n" "✓ $(($SECONDS - $start))s"
 }
 
 setup_hdf5 () {
+    start=$SECONDS
     echo ::group::"Download HDF5"
-    curl -s https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.1/src/hdf5-1.12.1.tar.gz -o hdf5-1.12.1.tar.gz
+    curl https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.1/src/hdf5-1.12.1.tar.gz -o hdf5-1.12.1.tar.gz
     tar -xf hdf5-1.12.1.tar.gz
     mkdir hdf5
     export HDF5_DIR="$PWD/hdf5"
     export HDF5_INCLUDEDIR="$HDF5_DIR/include"
     export HDF5_LIBDIR="$HDF5_DIR/lib"
     echo ::endgroup::
+
+    printf "%70.70s\n" "✓ $(($SECONDS - $start))s"
+    start=$SECONDS
 
     echo ::group::"Build HDF5"
     cd hdf5-1.12.1
@@ -56,6 +63,21 @@ setup_hdf5 () {
     make install
     cd ..
     echo ::endgroup::
+
+    printf "%70.70s\n" "✓ $(($SECONDS - $start))s"
+}
+
+setup_highfive () {
+    start=$SECONDS
+
+    echo ::group::"Download highfive"
+    curl -L https://github.com/BlueBrain/HighFive/archive/refs/tags/v2.3.1.tar.gz -o highfive-2.3.1.tar.gz
+    tar -xf highfive-2.3.1.tar.gz
+    mv highfive-2.3.1 highfive
+    export HIGHFIVE_INCLUDEDIR="$PWD/highfive/include"
+    echo ::endgroup::
+
+    printf "%70.70s\n" "✓ $(($SECONDS - $start))s"
 }
 
 cd ..
