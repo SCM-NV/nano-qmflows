f6f6155a10a53b49fe9769320c7dcbb3bfd78739
Bas van Beek <b.f.van.beek@vu.nl>
2022-09-05 13:24:57 +0200

    CI: Start testing using the CP2K 2022.1 release

--- scripts/download_cp2k.sh
+++ scripts/download_cp2k.sh
@@ -4,8 +4,16 @@ set -euo pipefail
 
 ARCH="$1"
 VERSION="$2"
-VERSION_LONG=v"$VERSION".0
+
+# After the 9.1 release CP2K switched to a `<year>.<suffix>` version scheme (e.g. `2021.1`)
+if [[ $VERSION =~ [0-9][0-9][0-9][0-9]\.[0-9]+ ]]; then
+    PLAT="Linux-gnu"
+    VERSION_LONG=v"$VERSION"
+else
+    PLAT="Linux"
+    VERSION_LONG=v"$VERSION".0
+fi
 
 echo "Installing CP2K $ARCH $VERSION binaries"
-curl -Lsf https://github.com/cp2k/cp2k/releases/download/$VERSION_LONG/cp2k-$VERSION-Linux-$ARCH.ssmp -o /usr/local/bin/cp2k.ssmp
+curl -Lsf https://github.com/cp2k/cp2k/releases/download/$VERSION_LONG/cp2k-$VERSION-$PLAT-$ARCH.ssmp -o /usr/local/bin/cp2k.ssmp
 chmod u+rx /usr/local/bin/cp2k.ssmp
